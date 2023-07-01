a810611d26900f38339a711a27b509a78f39f5f9
Matsvei Niaverau <matvej2002@gmail.com>
2023-07-01 17:14:30 +0700

    earth: Define Bootloader and Platform flags
    Change-Id: I6b47573cc492a03ced1a4b718b49cbef9630d8ac

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,5 +21,13 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := earth
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6768
+BOARD_HAS_MTK_HARDWARE := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/earth/BoardConfigVendor.mk
