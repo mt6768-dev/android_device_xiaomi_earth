0dbd0b463a04ed46da0dedc29cac0e228640f363
bengris32 <bengris32@protonmail.ch>
2023-07-18 19:47:40 +0100

    earth: Include `hardware/mediatek` Soong namespace
    Change-Id: I9028cec415568445e4bf6b2cb7cdf3f03e95154d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -72,7 +72,8 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/earth/earth-vendor.mk)
