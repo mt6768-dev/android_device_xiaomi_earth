308fe3a1203e1986935399bbf43311f23dd1fa63
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-03 15:38:55 +0100

    earth: Default to 4GB Dalvik heap configuration
    * Our libinit is missing on GSIs and DSUs
    
    Change-Id: Ie5d7c5e297a47c9d5587b075bd0641914d368f78

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # A/B
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl \
