efd00d5017c3d346d3fca2d8c701e075d0323ed4
Matsvei Niaverau <matvej2002@gmail.com>
2024-03-20 09:53:15 +0100

    earth: libinit: Add `aether`, `earth`, `earth_la` and `earth_p` variant info
    Change-Id: I07a5dc470ed1a0881783f44580d3d7d88f33f499

--- libinit/init_xiaomi_earth.cpp
+++ libinit/init_xiaomi_earth.cpp
@@ -9,6 +9,57 @@
 
 #include "vendor_init.h"
 
+static const variant_info_t aether_info = {
+    .hwc_value = "",
+    .sku_value = "aether",
+
+    .brand = "Redmi",
+    .device = "aether",
+    .marketname = "Redmi 12C",
+    .model = "22126RN91Y",
+    .build_fingerprint = "Redmi/aether_global/aether:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys"
+};
+
+static const variant_info_t earth_info = {
+    .hwc_value = "",
+    .sku_value = "earth",
+
+    .brand = "Redmi",
+    .device = "earth",
+    .marketname = "Redmi 12C",
+    .model = "22120RN86G",
+    .build_fingerprint = "Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys"
+};
+
+static const variant_info_t earth_la_info = {
+    .hwc_value = "",
+    .sku_value = "earth_la",
+
+    .brand = "Redmi",
+    .device = "earth",
+    .marketname = "Redmi 12C",
+    .model = "2212ARNC4L",
+    .build_fingerprint = "Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys"
+};
+
+static const variant_info_t earth_p_info = {
+    .hwc_value = "",
+    .sku_value = "earth_p",
+
+    .brand = "Poco",
+    .device = "earth",
+    .marketname = "POCO C55",
+    .model = "22127PC95G",
+    .build_fingerprint = "POCO/earth_p_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys"
+};
+
+static const std::vector<variant_info_t> variants = {
+    aether_info,
+    earth_info,
+    earth_la_info,
+    earth_p_info,
+};
+
 void vendor_load_properties() {
     search_variant(variants);
 }
